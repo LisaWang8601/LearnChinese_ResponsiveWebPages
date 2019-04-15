8f41b35f926726c61811bca81bbca7fc872d13a8
LisaWang8601 <suhuanwang71@gmail.com>
2019-04-15 09:56:36 -0400

    add images for github portofolio

---
+++ public/firebase.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCv84fWUZ9CMIDjhaPjG47Gz4U6wIPucjw",
+    authDomain: "learnchinese-lw.firebaseapp.com",
+    databaseURL: "https://learnchinese-lw.firebaseio.com",
+    projectId: "learnchinese-lw",
+    storageBucket: "learnchinese-lw.appspot.com",
+    messagingSenderId: "764445328163"
+};
