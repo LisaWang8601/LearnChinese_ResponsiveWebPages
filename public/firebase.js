d846970ad9fb2cb3a73ab8e7da7acd2ada8976e1
LisaWang8601 <44842317+LisaWang8601@users.noreply.github.com>
2019-04-15 19:03:49 -0400

    Delete firebase.js

--- public/firebase.js
+++
@@ -1,8 +0,0 @@
-export default {
-    apiKey: "AIzaSyCv84fWUZ9CMIDjhaPjG47Gz4U6wIPucjw",
-    authDomain: "learnchinese-lw.firebaseapp.com",
-    databaseURL: "https://learnchinese-lw.firebaseio.com",
-    projectId: "learnchinese-lw",
-    storageBucket: "learnchinese-lw.appspot.com",
-    messagingSenderId: "764445328163"
-};
